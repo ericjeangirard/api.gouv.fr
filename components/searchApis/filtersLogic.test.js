37c4ec51a77c7b8492b1829a6a848025148a4ca7
xavierJp <xajouppe@gmail.com>
2020-02-24 16:30:53 +0100

    correctly setup jest and include test in CI

--- components/searchApis/filtersLogic.test.js
+++ components/searchApis/filtersLogic.test.js
@@ -1,13 +1,25 @@
-const computeSearchResults = require('./filtersLogic');
+const filters = require('./filtersLogic');
 
 const time = (callBack, iteration = 1) => {
   const t0 = performance.now();
-  for (i = 0; i < iteration; i++) {
+  for (let i = 0; i < iteration; i++) {
     callBack();
   }
   return performance.now() - t0;
 };
 
-test('Performance', () => {
-  expect(time(() => computeSearchResults('heeey'), 10)).toBeLesserThan(3000);
-});
+test('Performance : 100 search should not exceed 300 milli seconds', () => {
+  const t = time(
+    () => filters.computeSearchResults(['France', 'France COnnect', 'Geo']),
+    100
+  );
+  expect(t).toBeLessThan(300);
+}, 4000);
+
+test('Random search : should never fail', () => {
+  const randomString = Math.random().toString(36);
+  console.log(randomString);
+  console.log(filters.computeSearchResults([randomString]));
+
+  expect(true).toBe(true);
+}, 600);
